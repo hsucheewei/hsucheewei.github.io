7d9db06563be34bf3aa7d0547292067c64125796
hsucheewei <hsucheewei@gmail.com>
2022-03-28 15:19:23 +0800

    remove

--- WD Large Coursework/Javascript/index.js
+++
@@ -1,8 +0,0 @@
-function burgerMenu() {
-    var x = document.getElementById("mobileLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
